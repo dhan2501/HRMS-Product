2dbde05c4d35d16fac26aee040810db3c2b6d79b
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2026-07-27 22:34:46 +0530

    work on Hiring process Module

--- hrms/settings.py
+++ hrms/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hrms-secret-key-change-in-production-2024'
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'hrms-product-production.up.railway.app']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'hrms-product-production.up.railway.app']
 # DEBUG = True
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = [
 #     "hrms-product-production.up.railway.app",
 #     "localhost",
