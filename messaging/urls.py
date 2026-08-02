89aeed8ccc3047d0461a7edfc1a1bce84d97b7e9
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2026-08-02 09:30:02 +0530

    i have made responsive chat bot according to devices

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,3 +1,16 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.chat_home, name='chat_home'),
+#     path('open/<int:user_id>/', views.open_chat, name='open_chat'),
+#     path('room/<int:conv_id>/', views.chat_room, name='chat_room'),
+#     path('room/<int:conv_id>/send/', views.send_message, name='send_message'),
+#     path('room/<int:conv_id>/poll/', views.poll_messages, name='poll_messages'),
+#     path('unread/', views.unread_count, name='unread_count'),
+#     path('group/create/', views.create_group, name='create_group'),
+# ]
+
 from django.urls import path
 from . import views
 
@@ -8,5 +21,6 @@ urlpatterns = [
     path('room/<int:conv_id>/send/', views.send_message, name='send_message'),
     path('room/<int:conv_id>/poll/', views.poll_messages, name='poll_messages'),
     path('unread/', views.unread_count, name='unread_count'),
+    path('list.json/', views.conv_list_json, name='conv_list_json'),
     path('group/create/', views.create_group, name='create_group'),
 ]
